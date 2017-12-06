5c119883ee086ff59d2dcd36392540052912ea99
Charistoph <christoph.bernkopf95@gmail.com>
2017-12-06 10:24:05 +0100

    working version matlab ml code with results saving

--- ml_matlab.m
+++ ml_matlab.m
@@ -6,14 +6,10 @@ if readdata
 end
 
 makedata = false;
-ml =       true;
-print =    false;
+ml =       false;
+print =    true;
 
 if makedata
-%  ntr=1
-%  ntr=17443
-
-  ntr
   inputs=zeros(ntr,72);
   targets=zeros(ntr,5);
 
@@ -47,13 +43,19 @@ if ml
   size(inputs)
   size(targets)
 
-  net=feedforwardnet(12); % regression network with a single hidden layer with 12 neurons
-%  net=fitnet(72,36,24,15,5);
+  ntr=size(inputs,2);
+%  ntr=1
+%  ntr=17443
 
-  traininputs=inputs(:,1:ntr*19/20);
-  traintargets=targets(:,1:ntr*19/20);
-  testinputs=inputs(:,ntr*19/20+1:end);
-  testtargets=targets(:,ntr*19/20+1:end);
+  targets(4:5,:)=rand(size(targets(4:5,:)))*10^-10;
+
+  net=feedforwardnet(12); % regression network with a single hidden layer with 12 neurons
+%  net=fitnet([72,36,24,15,5]);
+
+  traininputs=inputs(:,1:round(ntr*19/20));
+  traintargets=targets(:,1:round(ntr*19/20));
+  testinputs=inputs(:,round(ntr*19/20)+1:end);
+  testtargets=targets(:,round(ntr*19/20)+1:end);
 
 %  options = trainingOptions('sgdm','MiniBatchSize',64)
 
@@ -65,11 +67,18 @@ end
 
 if print
   targets=zeros(size(netout));
+  results=zeros(size(netout,1),2);
   res=testtargets-netout;
   for i=1:size(netout,1)
-    figure(i),clf,hist(res(i,:),50)
+    figure(i),clf,hist(res(i,:),50);
+    results(i,:)=[mean(res(i,:)),std(res(i,:))];
   end
 
-  [mean(res1) std(res1)]
-  [mean(res2) std(res2)]
+  saveas(figure(1),[pwd '/ml_output_matlab/figure_' num2str(1) '.fig']);
+  saveas(figure(2),[pwd '/ml_output_matlab/figure_' num2str(2) '.fig']);
+  saveas(figure(3),[pwd '/ml_output_matlab/figure_' num2str(3) '.fig']);
+  saveas(figure(4),[pwd '/ml_output_matlab/figure_' num2str(4) '.fig']);.csv
+  saveas(figure(5),[pwd '/ml_output_matlab/figure_' num2str(5) '.fig']);
+  save ml_output_matlab/results results
+  csvwrite('ml_output_matlab/results.csv',results)
 end
