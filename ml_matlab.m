6b5d50fae1e0db477e35eb95646c4be7bb7d9d02
PC McPcface <christoph@PCs-MacBook-Pro.local>
2018-02-06 11:49:43 +0100

    train with 48, 24

--- ml_matlab.m
+++ ml_matlab.m
@@ -29,10 +29,10 @@ for i=1:60
   targets=0;
 %  itr1=1+(i-1)*60000;
 %  itr2=60000+(i-1)*60000;
-%  itr1=1;
-%  itr2=10000*i;
-  itr1=220000;
-  itr2=220000+10000*i;
+  itr1=1;
+  itr2=10000*i;
+%  itr1=220000;
+%  itr2=220000+10000*i;
   inputs=inp(:,itr1:itr2);
   targets=targ(:,itr1:itr2);
 
@@ -49,8 +49,8 @@ for i=1:60
 
   % switch to test different methods
     if trainMethod == 1
-        neurons = 5
-%        neurons = [48,24]
+%        neurons = 5
+        neurons = [48,24]
         targets_train=targets(1:3,:);
         inputs_train=inputs;
         net=feedforwardnet(neurons)
