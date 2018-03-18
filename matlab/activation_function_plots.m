c9212df585a3ee46f754a546b446aa00ebbda025
PC McPcface <christoph@PCs-MacBook-Pro.local>
2018-03-18 23:57:02 +0100

    updated activation function plotter

--- matlab/activation_function_plots.m
+++ matlab/activation_function_plots.m
@@ -1,5 +1,5 @@
-one = false;
-two = true;
+one = true;
+two = false;
 
 if one
     x = -5:0.1:5;
@@ -17,21 +17,35 @@ if one
         y2(idx) = 1/(1+exp(-x(idx)));
     end
 
+    for idx = 1:numel(x)
+        if x(idx)<0
+            y3(idx) = 0;
+
+        else
+            y3(idx) = x(idx);
+        end 
+    end
+
     figure
-    subplot(1,3,1)       % add first plot in 2 x 1 grid
+    subplot(2,2,1)
     plot(x,y1,'red')
-    axis([-5 5 -1 1])
+    axis([-5 5 -1.5 1.5])
     title('1. Step Function')
 
-    subplot(1,3,2)       % add second plot in 2 x 1 grid
-    plot(x,y2,'red')       % plot using + markers
-    axis([-5 5 -1 1])
+    subplot(2,2,2)
+    plot(x,y2,'red')
+    axis([-5 5 -1.5 1.5])
     title('2. Sigmoid')
 
-    subplot(1,3,3)       % add second plot in 2 x 1 grid
-    plot(x,tanh(x),'red')       % plot using + markers
-    axis([-5 5 -1 1])
+    subplot(2,2,3)
+    plot(x,tanh(x),'red')
+    axis([-5 5 -1.5 1.5])
     title('3. Hyperbolic Tangent')
+
+    subplot(2,2,4)
+    plot(x,y3,'red')
+    axis([-5 5 -1.5 1.5])
+    title('4. RELU')
 end
 
 if two
@@ -43,13 +57,13 @@ if two
     end 
 
     figure
-    subplot(1,2,1)       % add first plot in 2 x 1 grid
+    subplot(1,2,1)
     plot(x,exp(-x/20),'red')
     axis([0 100 0 1])
     title('1. Desired Loss')
 
-    subplot(1,2,2)       % add second plot in 2 x 1 grid
-    plot(x,y,'red')       % plot using + markers
+    subplot(1,2,2)
+    plot(x,y,'red')
     axis([0 100 0 1])
     title('2. Overfitted Loss')
 end
